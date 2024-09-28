4e34e3313f2fe88f200629d27fbc7a3f3d8a659f
aodavit-d <davit@aobyte.com>
2024-09-28 18:40:43 +0400

    Added missing routes; ordered by name

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import { PORT } from "./constants";
 import categoriesRouter from "./src/routers/categories";
-import servicesRouter from "./src/routers/services";
 import locationsRouter from "./src/routers/locations";
 import providersRouter from "./src/routers/providers";
+import providerServicesRouter from "./src/routers/providerServices";
+import servicesRouter from "./src/routers/services";
+import subCategoriesRouter from "./src/routers/subCategories";
+import usersRouter from "./src/routers/users";
 
 const app = express();
 app.use(express.json());
 
 app.use("/categories", categoriesRouter);
-app.use("/services", servicesRouter);
 app.use("/locations", locationsRouter);
 app.use("/providers", providersRouter);
+app.use("/provider-services", providerServicesRouter);
+app.use("/services", servicesRouter);
+app.use("/sub-categories", subCategoriesRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Express is listening at http://localhost:${PORT}`);
